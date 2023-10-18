9cdeea801a40dc339f266ba08327724ff038ca6b
xquery <best.highloyal@gmail.com>
2023-10-18 20:54:24 +0300

    Made footer and finished website layout

--- pages/HomePage/index.tsx
+++ pages/HomePage/index.tsx
@@ -4,6 +4,7 @@ import styles from './HomePage.module.scss'
 import {ProductGrid} from "@/components/ProductGrid";
 import {Catalog} from "@/components/Catalog";
 import {SpecialProducts} from "@/components/SpecialProducts";
+import {Footer} from "@/components/Footer";
 
 export const HomePage = () => {
     return (
@@ -12,6 +13,7 @@ export const HomePage = () => {
             <ProductGrid />
             <Catalog />
             <SpecialProducts />
+            <Footer />
         </div>
     );
 };
