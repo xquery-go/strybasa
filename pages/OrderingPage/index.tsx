dab4546e9f5d021950022a20221c77403c707851
xquery <best.highloyal@gmail.com>
2023-12-18 16:47:48 +0300

    Finished ordering page

--- pages/OrderingPage/index.tsx
+++ pages/OrderingPage/index.tsx
@@ -15,7 +15,7 @@ export const OrderingPage = ({ token }: { token: string }) => {
             signer_address: '',
         },
         onSubmit: (values) => {
-            console.log("Submit")
+            console.log("Submit", values)
         }
     })
     return (
@@ -23,24 +23,72 @@ export const OrderingPage = ({ token }: { token: string }) => {
             <Header />
             <div className={styles.content}>
                 <h2 className={`${styles.name} ${roboto.className}`}>Оформление заказа</h2>
-                <form className={styles.form} onSubmit={formik.handleSubmit}>
-                    <div className={styles.field}>
-                        <label htmlFor="signer_lastname" className={styles.label}>Фамилия</label>
-                        <input name="signer_lastname" className={styles.input} type="text" placeholder={'Фамилия'} />
+                <div className={styles.grid}>
+                    <form className={styles.form} onSubmit={formik.handleSubmit}>
+                        <div className={styles.field}>
+                            <label htmlFor="signer_lastname" className={styles.label}>Фамилия</label>
+                            <input
+                                name="signer_lastname"
+                                className={styles.input}
+                                type="text"
+                                placeholder={'Фамилия'}
+                                onChange={formik.handleChange}
+                                value={formik.values.signer_lastname}
+                            />
+                        </div>
+                        <div className={styles.field}>
+                            <label htmlFor="signer_firstname" className={styles.label}>Имя</label>
+                            <input
+                                name="signer_firstname"
+                                className={styles.input}
+                                type="text"
+                                placeholder={'Имя'}
+                                onChange={formik.handleChange}
+                                value={formik.values.signer_firstname}
+                            />
+                        </div>
+                        <div className={styles.field}>
+                            <label htmlFor="signer_address" className={styles.label}>Адрес</label>
+                            <input
+                                name="signer_address"
+                                className={styles.input}
+                                type="text"
+                                placeholder={'Адрес'}
+                                onChange={formik.handleChange}
+                                value={formik.values.signer_address}
+                            />
+                        </div>
+                        <div className={styles.field}>
+                            <label htmlFor="order_comment" className={styles.label}>Комментарий к заказу</label>
+                            <input
+                                name="order_comment"
+                                className={styles.input}
+                                type="text"
+                                placeholder={'Комментарий к заказу'}
+                                onChange={formik.handleChange}
+                                value={formik.values.order_comment}
+                            />
+                        </div>
+                        <button type={'submit'} className={styles.btn}>Оформить</button>
+                    </form>
+                    <div className={styles.info}>
+                        <p className={`${styles.info_title} ${roboto.className}`}>Информация по оформлению</p>
+                        <ul className={styles.info_list}>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in pretium magna.
+                                Ut quis orci a risus mollis tempor id quis mi. Aliquam porta sem congue ligula finibus,
+                                malesuada luctus sapien facilisis. Sed consequat velit vitae lectus volutpat elementum.
+                                In hac habitasse platea dictumst. Vestibulum rutrum nisl erat, eu consequat lorem
+                                rhoncus quis. </li>
+                            <li>Donec porttitor, diam sed vulputate tincidunt, augue nunc facilisis nibh, ac luctus nisl
+                                nibh ac dui. Praesent porta nisl vel sapien consequat sollicitudin. Fusce eros magna,
+                                semper ut odio accumsan, rhoncus commodo nisi. Integer a rhoncus nibh, ut molestie
+                                ipsum. </li>
+                            <li>Sed aliquam diam blandit, rutrum nisl at, eleifend diam. Curabitur
+                                vestibulum, augue in mollis dapibus, tellus sem volutpat diam, quis condimentum est
+                                lectus at ligula. Ut eget dapibus sem. Proin id lobortis dolor.</li>
+                        </ul>
                     </div>
-                    <div className={styles.field}>
-                        <label htmlFor="signer_firstname" className={styles.label}>Имя</label>
-                        <input name="signer_firstname" className={styles.input} type="text" placeholder={'Имя'} />
-                    </div>
-                    <div className={styles.field}>
-                        <label htmlFor="signer_address" className={styles.label}>Адрес</label>
-                        <input name="signer_address" className={styles.input} type="text" placeholder={'Адрес'} />
-                    </div>
-                    <div className={styles.field}>
-                        <label htmlFor="order_comment" className={styles.label}>Комментарий к заказу</label>
-                        <input name="order_comment" className={styles.input} type="text" placeholder={'Комментарий к заказу'} />
-                    </div>
-                </form>
+                </div>
             </div>
             <Footer />
         </div>
