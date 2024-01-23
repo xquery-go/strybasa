bc6012f9f8bc016e65f45e669e91a606d434f639
xquery <best.highloyal@gmail.com>
2024-01-23 17:02:11 +0300

    Fix bags with fonts

--- config/fonts/fonts.ts
+++ config/fonts/fonts.ts
@@ -1,12 +1,6 @@
 import { Alumni_Sans, Roboto_Mono, Roboto } from "next/font/google";
 import localFont from "next/font/local";
 
-<<<<<<< HEAD
 export const roboto = Roboto({weight: ['400', '500', '700'], subsets: ['latin', 'cyrillic'], fallback: []})
 export const alumniSans = Alumni_Sans({ subsets: ['latin', 'cyrillic'], fallback: [] })
 export const robotoMono = Roboto_Mono({ subsets: ['latin', 'cyrillic'], weight: ['400', '500', '600', '700'], fallback: [] })
-=======
-export const roboto = Roboto({weight: ['400', '500', '700'], subsets: ['latin', 'cyrillic'],fallback: []})
-export const alumniSans = Alumni_Sans({ subsets: ['latin', 'cyrillic'],fallback: [] })
-export const robotoMono = Roboto_Mono({ subsets: ['latin', 'cyrillic'], weight: ['400', '500', '600', '700'],fallback: [] })
->>>>>>> e2c520f5e87f19342316f66a3c31e54bb80e29d1
