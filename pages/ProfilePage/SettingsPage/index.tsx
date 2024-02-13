d4644e564205926406e0088f7483d319e94985b4
xquery <best.highloyal@gmail.com>
2024-02-13 12:11:13 +0300

    Make settings links/btns

--- pages/ProfilePage/SettingsPage/index.tsx
+++ pages/ProfilePage/SettingsPage/index.tsx
@@ -1,35 +1,57 @@
-'use client';
-import React, {useEffect} from 'react';
+'use client'
+import useProfileStore from '@/app/ProfileStore'
+import { useUserStore } from '@/app/userStore'
+import { roboto } from '@/config/fonts/fonts'
+import { LogOut, PenLine, RotateCcw } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import styles from './SettingsPage.module.scss'
-import useProfileStore from "@/app/ProfileStore";
-import {roboto} from "@/config/fonts/fonts";
-import {LogOut} from "lucide-react";
-import {useUserStore} from "@/app/userStore";
-import {useRouter} from "next/navigation";
 
 const SettingsPage = () => {
-    const { setCurTab } = useProfileStore()
-    const {quitAccount} = useUserStore()
-    const router = useRouter()
-    useEffect(() => {
-        setCurTab("settings")
-    }, [setCurTab])
-    return (
-        <div>
-            <title>Настройки | Стройбаза Тиски</title>
-            <h1 className={`${styles.name} ${roboto.className}`}>Настройки аккаунта</h1>
-            <button
-                className={styles.link}
-                onClick={() => {
-                    quitAccount()
-                    router.push('/')
-                }}
-            >
-                <>Выйти из аккаунта</>
-                <LogOut color={'#FFFFFF'} width={20} height={20}/>
-            </button>
-        </div>
-    )
+	const { setCurTab } = useProfileStore()
+	const { quitAccount } = useUserStore()
+	const router = useRouter()
+	useEffect(() => {
+		setCurTab('settings')
+	}, [setCurTab])
+	return (
+		<div>
+			<title>Настройки | Стройбаза Тиски</title>
+			<h1 className={`${styles.name} ${roboto.className}`}>
+				Настройки аккаунта
+			</h1>
+			<div className={styles.btns}>
+				<button
+					className={styles.link}
+					onClick={() => {
+						quitAccount()
+						router.push('/')
+					}}
+				>
+					<span className={styles.btn_text}>Выйти из аккаунта</span>
+					<LogOut color={'#FFFFFF'} width={20} height={20} />
+				</button>
+				<button
+					className={`${styles.link} ${styles.change_btn}`}
+					onClick={() => {
+						router.push('/change_password')
+					}}
+				>
+					<span className={styles.btn_text}>Изменить пароль</span>
+					<PenLine color={'#FFFFFF'} width={20} height={20} />
+				</button>
+				<button
+					className={`${styles.link} ${styles.recovery_btn}`}
+					onClick={() => {
+						router.push('/password_recovery')
+					}}
+				>
+					<span className={styles.btn_text}>Восстановить пароль</span>
+					<RotateCcw color={'#FFFFFF'} width={20} height={20} />
+				</button>
+			</div>
+		</div>
+	)
 }
 
-export default SettingsPage;
+export default SettingsPage
